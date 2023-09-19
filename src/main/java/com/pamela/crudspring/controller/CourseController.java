a718ba314757b7a8474a0c2dbeaa92e04a35c9fb
Pamela Albuquerque <artemis121289@gmail.com>
2023-09-18 22:17:21 -0300

    Buscar de cursos pr Id

--- src/main/java/com/pamela/crudspring/controller/CourseController.java
+++ src/main/java/com/pamela/crudspring/controller/CourseController.java
@@ -2,7 +2,7 @@ package com.pamela.crudspring.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import com.pamela.crudspring.model.CourseModel;
 
@@ -22,9 +22,18 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseModel> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public CourseModel create(@RequestBody CourseModel courseModel) {
         return courseRepository.save(courseModel);
     }
 }
+
